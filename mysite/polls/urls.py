4e9e8d2b2456d874d45bc33f2b48c008ec0d9da6
gabbyramos <gabbyanoob@gmail.com>
2021-01-19 00:23:38 -0300

    Inicio da aplicaçao Django

---
+++ mysite/polls/urls.py
@@ -0,0 +1,10 @@
+# from importlib.resources import path
+#
+# from django.conf.urls import url
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
